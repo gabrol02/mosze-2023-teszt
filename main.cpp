0b896d23ddcb8b2b01c64a93c1342e354d292427
gabrol02 <gbneuberger@gmail.com>
2023-09-22 10:23:34 +0200

    Hibák javítva

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS]; // átnevezés NELEMENTS-rõl N_ELEMENTS re
+    std::cout << '1-100 ertekek duplazasa'; // ";" pótlása
+    for (int i = 0;i<N_ELEMENTS;i++) // for ciklus helyes paraméterezése
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)  // for ciklus helyes paraméterezése
     {
         std::cout << "Ertek:"
     }    
@@ -22,5 +22,8 @@ int main()
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+
+    delete[] b; // Fel kell szabadítani a dinamikus memóriát
+
     return 0;
 }
